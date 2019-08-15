65ae61952da13c68c567934a7a0ead7a814c092c
James Strieter <jamesstrieter@MacBook-Air-2.local>
2019-08-15 10:23:06 -0700

    Refactored code to make more sense.

--- VhdlParser.py
+++ VhdlParser.py
@@ -16,15 +16,16 @@ def is_int(x):
         return False
 
 
-def make_one_port(name, dir, dtype, width):
+def make_one_port(name, dir, dtype):
     assert(dir == "in" or dir == "out" or dir == "inout")
     return {
         "name": name,
         "direction": dir,
         "datatype": dtype,
-        "width": width,
+        "width": "1",
+        "default_value": "",
         "is_port": True,
-        "is_generic": False
+        "is_generic": False,
     }
 
 
@@ -40,67 +41,167 @@ def make_one_generic(name, dtype, width):
     }
 
 
+def extract_ports(short_list):
+    ports = []
+    paren_depth = 0
+    state = 0
+    state_history = [0] * 100
+    paren_depth_history = [0] * 100
+    new_port = None
+    enable_width_detection = True
+    enable_default_value_detection = True
+    this_default = ""
+    require_more_tokens = False
+    num_tokens_counted = 0
+    for shrt_token in short_list:
+        # print("token:", shrt_token, "       depth: ", paren_depth)
+        if shrt_token == "(":
+            paren_depth += 1
+        elif shrt_token == ")":
+            paren_depth -= 1
+        elif shrt_token == ";":
+            state = 0
+            this_name = "reset name"
+            this_width = "1"
+            this_direction = "reset dir"
+            this_type = "reset type"
+            this_default = ""
+            enable_width_detection = True
+            enable_default_value_detection = True
+            require_more_tokens = False
+            num_tokens_counted = 0
+        elif shrt_token == ":":
+            state = 1
+        elif shrt_token == "downto":
+            state = 100
+        elif shrt_token == ":=":
+            state = 200
+        else:
+            if state == 0:
+                this_name = shrt_token
+            elif state == 1:
+                this_direction = shrt_token
+                state = 2
+            elif state == 2:
+                this_type = shrt_token
+                new_port = make_one_port(this_name, this_direction, this_type)
+                ports.append(new_port)
+                state = 3
+            elif state == 3:
+                if enable_width_detection:
+                    this_width = shrt_token
+                    if is_int(this_width):
+                        this_width = str(int(this_width) + 1)
+                    enable_width_detection = False
+                    new_port["width"] = this_width
+            elif state == 100:
+                pass
+            elif state == 200:
+                if enable_default_value_detection:
+                    num_tokens_counted = 0
+                    this_default = shrt_token
+                    if shrt_token == "others":
+                        require_more_tokens = True
+                    else:
+                        require_more_tokens = False
+                    enable_default_value_detection = False
+                    new_port["default_value"] = this_default
+                elif require_more_tokens:
+                    if num_tokens_counted < 3:
+                        new_port["default_value"] += shrt_token
+
+        if shrt_token == ";" and paren_depth == 0:
+            break
+
+        state_history.append(state)
+        paren_depth_history.append(paren_depth)
+    return ports
+
+
+def extract_generics(short_list):
+    generics = []
+    paren_depth = 0
+    state = 0
+    state_history = [0] * 100
+    paren_depth_history = [0] * 100
+    new_port = None
+    enable_width_detection = True
+    enable_default_value_detection = True
+    this_default = ""
+    require_more_tokens = False
+    num_tokens_counted = 0
+    for shrt_token in short_list:
+        # print("token:", shrt_token, "       depth: ", paren_depth)
+        if shrt_token == "(":
+            paren_depth += 1
+        elif shrt_token == ")":
+            paren_depth -= 1
+        elif shrt_token == ";":
+            state = 0
+            this_name = "reset name"
+            this_width = "1"
+            this_type = "reset type"
+            this_default = ""
+            enable_width_detection = True
+            enable_default_value_detection = True
+            require_more_tokens = False
+            num_tokens_counted = 0
+        elif shrt_token == ":":
+            state = 1
+        elif shrt_token == ":=":
+            state = 200
+        elif shrt_token == "downto":
+            state = 100
+        else:
+            if state == 0:
+                this_name = shrt_token
+            elif state == 1:
+                state = 2
+            elif state == 2:
+                this_type = shrt_token
+                new_generic = make_one_generic(this_name, this_type)
+                generics.append(new_generic)
+                state = 3
+            elif state == 3:
+                if enable_width_detection:
+                    this_width = shrt_token
+                    if is_int(this_width):
+                        this_width = str(int(this_width) + 1)
+                    enable_width_detection = False
+                    new_generic["width"] = this_width
+            elif state == 100:
+                pass
+            elif state == 200:
+                if enable_default_value_detection:
+                    num_tokens_counted = 0
+                    this_default = shrt_token
+                    if shrt_token == "others":
+                        require_more_tokens = True
+                    else:
+                        require_more_tokens = False
+                    enable_default_value_detection = False
+                    new_generic["default_value"] = this_default
+                elif require_more_tokens:
+                    if num_tokens_counted < 3:
+                        new_generic["default_value"] += shrt_token
+
+        if shrt_token == ";" and paren_depth == 0:
+            break
+
+        state_history.append(state)
+        paren_depth_history.append(paren_depth)
+    return generics
+
+
 def scan_one_file(filename):
     with open(filename, 'r') as f:
         bigstr = f.read()
         bigstr = guarantee_whitespace(bigstr)
         los = bigstr.split()
-        ports = []
-        generics = []
         for i in range(len(los)):
             one_tok = los[i]
             if one_tok == "port":
-                short_list = los[i:]
-                paren_depth = 0
-                state = 0
-                state_history = [0]*100
-                paren_depth_history = [0]*100
-                new_port = None
-                enable_width_detection = True
-                for shrt_token in short_list:
-                    # print("token:", shrt_token, "       depth: ", paren_depth)
-                    if shrt_token == "(":
-                        paren_depth += 1
-                    elif shrt_token == ")":
-                        paren_depth -= 1
-                    elif shrt_token == ";":
-                        state = 0
-                        this_name = "reset name"
-                        this_width = "1"
-                        this_direction = "reset dir"
-                        this_type = "reset type"
-                        enable_width_detection = True
-                    elif shrt_token == ":":
-                        state = 1
-                    elif shrt_token == ":=":
-                        state = 200
-                    elif shrt_token == "downto":
-                        state = 100
-                    else:
-                        if state == 0:
-                            this_name = shrt_token
-                        elif state == 1:
-                            this_direction = shrt_token
-                            state = 2
-                        elif state == 2:
-                            this_type = shrt_token
-                            new_port = make_one_port(this_name, this_direction, this_type, "1")
-                            ports.append(new_port)
-                            state = 3
-                        elif state == 3:
-                            if enable_width_detection:
-                                this_width = shrt_token
-                                if is_int(this_width):
-                                    this_width = str(int(this_width) + 1)
-                                enable_width_detection = False
-                                new_port["width"] = this_width
-                    # if (state == 0 and prev_state != 0) or (paren_depth == -1):
-                    if shrt_token == ";" and paren_depth == 0:
-                        break
-
-                    state_history.append(state)
-                    paren_depth_history.append(paren_depth)
-
+                ports = extract_ports(los[i:])
             elif one_tok == "port":
                 short_list = los[i:]
 
@@ -110,7 +211,7 @@ def scan_one_file(filename):
 
 
 if __name__ == "__main__":
-    for one_port in scan_one_file("./vhdl_literals/dff_fast.vhd"):
+    for one_port in scan_one_file("./vhdl_literals/vhd_test.vhd"):
         print(str(one_port))
 
 
